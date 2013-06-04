bc10c105b1e2e4b1650400570cb31393a75f8e3c
scarface <juancarlos@laurelshoes.com>
2013-06-04 18:42:58 +0200

    github upload

---
+++ prueba.java
@@ -0,0 +1,12 @@
+class prueba {
+
+
+  public static void main(String[] args) {
+    System.out.println("Hola mundo");
+    otro operacion = new otro();
+    int result = operacion.jander(2,3);
+    System.out.println(result);
+  }
+
+
+}
